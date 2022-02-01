0fa10475a0a10e87959646d6bd7750e11fe7fff8
Pranav <naringrekarpranav6@live.com>
2022-02-01 22:28:58 +0530

    D7 - Express Start

---
+++ Notes/notes-section-6.js
@@ -0,0 +1,57 @@
+/*
+# SECTION 6 - EXPRESS: LET'S START BUILDING THE NATOURS API!
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+#   48. What is Express?
+#   49. Installing Postman
+#   50. Setting up Express and Basic Routing
+#   51. APIs and RESTful API Design
+#   52. Starting Our API: Handling GET Requests
+#   53. Handling POST Requests
+#   54. Responding to URL Parameters
+#   55. Handling PATCH Requests
+#   56. Handling DELETE Requests
+#   57. Refactoring Our Routes
+#   58. Middleware and the Request-Response Cycle
+#   59. Creating Our Own Middleware
+#   60. Using 3rd-Party Middleware
+#   61. Implementing the "Users" Routes
+#   62. Creating and Mounting Multiple Routers
+#   63. A Better File Structure
+#   64. Param Middleware
+#   65. Chaining Multiple Middleware Functions
+#   66. Serving Static Files
+#   67. Environment Variables
+#   68. Setting up ESLint + Prettier in VS Code
+
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+#   48. What is Express?
+
+-> 👉    Express is a minimal node.js framework, a higher level of abstraction.
+-> 👉    Express contains a very robust set of features: complex routing, easier handling of requests and responses, middleware, server-side rendering etc.
+-> 👉    Express allows for rapid development of node.js applications: we don't have to re-invent the wheel
+-> 👉    Express makes it easier to organize our application into the MVC architecture
+
+>> It is 100% written in node.js
+>> It is the most popular framework of node.js
+
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+#   49. Installing Postman
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+
+*/
