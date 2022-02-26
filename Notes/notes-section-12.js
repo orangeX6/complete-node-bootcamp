de54bad833cc16813bd19c5cc0f2a6fc27037a50
Pranav <naringrekarpranav6@live.com>
2022-02-27 04:31:43 +0530

    Section 12 Start

---
+++ Notes/notes-section-12.js
@@ -0,0 +1,60 @@
+/*
+
+# SECTION 12 - SERVER-SIDE RENDERING WITH PUG TEMPLATES
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+
+
+# ⎛⎝(•‿•)⎠⎞
+#   174. Section Intro
+#   175. Recap: Server-Side vs Client-Side Rendering
+#   176. Setting up Pug in Express
+#   177. First Steps with Pug
+#   178. Creating Our Base Template
+#   179. Including Files into Pug Templates
+#   180. Extending Our Base Template with Blocks
+#   181. Setting up the Project Structure
+#   182. Building the Tour Overview - Part 1
+#   183. Building the Tour Overview - Part 2
+#   184. Building the Tour Page - Part 1
+#   185. Building the Tour Page - Part 2
+#   186. Including a Map with Mapbox - Part 1
+#   187. Including a Map with Mapbox - Part 2
+#   188. Building the Login Screen
+#   189. Logging in Users with Our API - Part 1
+#   190. Logging in Users with Our API - Part 2
+#   191. Logging in Users with Our API - Part 3
+#   192. Logging out Users
+#   193. Rendering Error Pages
+#   194. Building the User Account Page
+#   195. Updating User Data
+#   196. Updating User Data with Our API
+#   197. Updating User Password with Our API
+
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+
+#   175. Recap: Server-Side vs Client-Side Rendering
+
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+//////////////////////////////////////////////////
+
+#   176. Setting up Pug in Express
+#   177. First Steps with Pug
+#   178. Creating Our Base Template
+#   179. Including Files into Pug Templates
+#   180. Extending Our Base Template with Blocks
+#   181. Setting up the Project Structure
+#   182. Building the Tour Overview - Part 1
+#   183. Building the Tour Overview - Part 2
+#   184. Building the Tour Page - Part 1
+#   185. Building the Tour Page - Part 2
+#   186. Including a Map with Mapbox - Part 1
+#   187. Including a Map with Mapbox - Part 2
+
+*/
